e8314fb2e626786c124fbac27b4534cc1927796c
Michal Stanek <michal.stanek@rossum.ai>
2023-05-14 20:00:53 +0200

    Add contact and about pages

--- src/simple_blog/views.py
+++ src/simple_blog/views.py
@@ -3,3 +3,11 @@ from django.http import HttpRequest, HttpResponse
 
 def home_page(request: HttpRequest) -> HttpResponse:
     return HttpResponse("<h1>Hello World</h1>")
+
+
+def contact_page(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("<h1>Our contact</h1>")
+
+
+def about_page(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("<h1>About the page</h1>")
